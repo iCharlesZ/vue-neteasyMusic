e228e25562427521e82e499670d33b7f33455fc4
ZhcLo <zhangchixx8@gmail.com>
2018-07-01 19:29:09 +0800

    my music sheet

--- src/components/findrecommend/findrecommend.vue
+++ src/components/findrecommend/findrecommend.vue
@@ -4,8 +4,22 @@
 			<swiper-slide v-for="(item, index) in items" :key="index">
 				<img class="image" :src="item" />
 			</swiper-slide>
-		</swiper>
-		<div class="swiper-pagination" slot="pagination"></div>  
+			<div class="swiper-pagination" slot="pagination"></div> 
+		</swiper> 
+		<div class="recommend-type">
+        	<div class="div-type">
+        		<i class="icon-type icon-fm"></i>
+        		<p class="title">私人FM</p>
+        	</div>
+        	<div class="div-type">
+        		<i class="icon-type icon-date"></i>
+        		<p class="title">每日听歌推荐</p>
+        	</div>
+        	<div class="div-type">
+        		<i class="icon-type icon-rank-list"></i>
+        		<p class="title">云音乐热歌榜</p>
+        	</div>
+        </div>
 	</div>
 </template>
 
@@ -70,5 +84,40 @@
 				.swiper-slide
 					.image
 						width:100%
+		.recommend-type
+			width:100%
+			height:90px
+			display:flex
+			align-items:center
+			.div-type
+				flex:1
+				height:100%
+				width:33.333333%
+				font-size:0
+				display:flex
+				flex-direction: column
+				justify-content: center
+				align-items: center
+				padding:0 10px
+				.icon-type
+					display:inline-block
+					color: #C62F2F
+					font-size:32px
+					width:50px
+					height:50px
+					line-height:50px
+					text-align:center
+					border:1px solid #C62F2F
+					border-radius:50%
+				.title
+					font-size:12px
+					font-weight:400
+					margin:0
+					margin-top:8px
+					text-overflow:ellipsis
+					overflow:hidden
+					white-space:nowrap
+					width:100%
+					text-align:center
 		
 </style>
--- src/components/musicsheet/musicsheet.vue
+++ src/components/musicsheet/musicsheet.vue
@@ -1,13 +1,123 @@
 <template>
 	<div class="musicsheet">
-		<div class="title"></div>
+		<div class="title">
+			<i class="toggle icon-down" ref="toggleicon"></i>
+			<div class="detail">
+				<span class="name">我创建的歌单</span>
+				<span class="count">(1)</span>
+				<i class="setting icon-setting"></i>
+			</div>
+		</div>
+		<div>
+			<div class="content">
+				<img class="sheetimg" alt="">
+				<div class="detail">
+					<p class="name">我喜欢的音乐</p>
+					<p class="count">1首歌曲</p>
+					<i class="setting icon-list-circle"></i>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-export default{
+	export default{
 
-}
+	}
 </script>
 
-<style></style>
+<style lang="stylus" rel="stylesheet/stylus">
+	.musicsheet
+		height:auto
+		width:100%
+		background:#fff
+		.title
+			display:block
+			height:28px
+			line-height:28px
+			background:#e1e1e1
+			position:relative
+			.toggle
+				position:absolute
+				top:50%
+				left:10px
+				font-size:14px
+				color:#666
+				transition:all 0.5s;
+				transform:translate3d(0,-50%,0)
+			.detail
+				margin-left:35px
+				color:#666
+				.name
+					display:inline-block
+					vertical-align:middle
+					font-weight:400
+					font-size:12px
+				.count
+					display:inline-block
+					vertical-align:middle
+					font-weight:300
+					font-size:12px
+				.setting
+					font-size:16px
+					position:absolute
+					width: 28px;
+					line-height: 28px;
+					text-align: center;
+					height: 28px;
+					top:50%
+					right:10px
+					transform:translate(0,-50%)
+		.content
+			height:60px
+			width:100%
+			box-sizing:border-box
+			position:relative
+			display:flex
+			&:active
+				background:$list_active
+			.sheetimg
+				margin:5px
+				width:50px
+				height:50px
+				flex:0 0 50px
+			.detail
+				height:60px
+				padding-left:5px
+				flex:1 1 auto
+				box-sizing:border-box
+				position:relative
+				.name,.count
+					width:80%
+					text-overflow:ellipsis
+					overflow:hidden
+					white-space:nowrap
+					margin:0
+					height:25px
+					line-height:25px
+				.name
+					margin-top:5px
+					color:#333
+					font-size:14px
+				.count
+					margin-bottom:5px
+					font-weight:400
+					font-size:12px
+					color:#666;
+				.border-1px
+					margin:0
+					border-1px($border_1px)
+				.setting
+					position:absolute
+					right:5px
+					width:36px
+					height:36px
+					line-height:36px
+					text-align:center
+					top:50%
+					color:$icon_color
+					transform:translate3d(0,-50%,0)
+					&:active
+						background:$list_active
+</style>
