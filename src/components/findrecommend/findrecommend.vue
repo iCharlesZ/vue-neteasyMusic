5545d0738415d2dcef9a74ab278403301d486906
ZhcLo <zhangchixx8@gmail.com>
2018-07-01 19:10:07 +0800

    vue-awesome-swiper

--- src/components/findrecommend/findrecommend.vue
+++ src/components/findrecommend/findrecommend.vue
@@ -1,13 +1,55 @@
 <template>
 	<div class="findrecommend">
-		<p>推荐</p>
+		<swiper :options="swiperOption" ref="mySwiper">
+			<swiper-slide v-for="(item, index) in items" :key="index">
+				<img class="image" :src="item" />
+			</swiper-slide>
+		</swiper>
+		<div class="swiper-pagination" slot="pagination"></div>  
 	</div>
 </template>
 
 <script>
-export default{
+	import 'swiper/dist/css/swiper.css'
+	import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
-}
+	export default{
+		data() {
+			return {
+				items:[
+					'/static/images/swiper/taylor-swift.jpg',
+					'/static/images/swiper/xusong.jpg',
+					'/static/images/swiper/linjunjie.jpg'
+				],
+				swiperOption: {
+					autoplay: 3000,
+					grabCursor: true,
+					setWrapperSize: true,
+					autoHeight: true,
+					pagination: '.swiper-pagination',
+					paginationClickable: true,
+					mousewheelControl: true,
+					autoplayDisableOnInteraction: false,
+					observeParents: true,
+					loop: true,
+				}
+			}
+		},
+		computed: {
+			swiper() {
+				return this.$refs.mySwiper.swiper
+			}
+		},
+		components: {
+            swiper,
+            swiperSlide
+        },
+        mounted() {
+			// 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
+			// this.swiper.slideTo(3, 1000, false)
+			console.log('mounted');
+		}
+	}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -22,4 +64,11 @@ export default{
 		z-index:11
 		background:#fff
 		-webkit-overflow-scrolling:touch
+		.swiper-container
+			position:relative
+			.swiper-wrapper
+				.swiper-slide
+					.image
+						width:100%
+		
 </style>
